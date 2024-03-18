626f9c83204ee683fb1e4640b8666da5ec879537
sunby <sunbingyi1992@gmail.com>
2024-03-18 21:44:13 +0800

    1
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- internal/core/src/segcore/Utils.cpp
+++ internal/core/src/segcore/Utils.cpp
@@ -799,10 +799,14 @@ LoadFieldDatasFromRemote(const std::vector<std::string>& remote_files,
         for (const auto& file : remote_files) {
             auto future = pool.Submit([&]() {
                 auto fileSize = rcm->Size(file);
-                LOG_ERROR("[remove me] file size: {}, file: {}", fileSize, file);
+                LOG_ERROR(
+                    "[remove me] file size: {}, file: {}", fileSize, file);
                 auto buf = std::shared_ptr<uint8_t[]>(new uint8_t[fileSize]);
                 rcm->Read(file, buf.get(), fileSize);
                 auto result = storage::DeserializeFileData(buf, fileSize);
+                LOG_ERROR("[remove me] result size: {}, file: {}",
+                          result->Size(),
+                          file);
                 return result->GetFieldData();
             });
             futures.emplace_back(std::move(future));
