a75cf88eb1e7a3ba5ea99677f6cd220d9ecaaccd
Zhen Ye <chyezh@outlook.com>
2026-06-10 17:56:38 +0800

    42794 streaming transform subscribe (#34)
    * enhance: document and proto
    
    Signed-off-by: chyezh <chyezh@outlook.com>
    
    * enhance: refine transform log subscription design (#42794)
    
    Signed-off-by: chyezh <chyezh@outlook.com>
    
    * feat: update transform subscription design (#42794)
    
    Signed-off-by: chyezh <chyezh@outlook.com>
    
    * feat: refine transform log subscription layering (#42794)
    
    Signed-off-by: chyezh <chyezh@outlook.com>
    
    * feat: add transform log subscription (#42794)
    
    Signed-off-by: chyezh <chyezh@outlook.com>
    
    * feat: add resumable transform log scanner (#42794)
    
    Signed-off-by: chyezh <chyezh@outlook.com>
    
    * feat: reuse transform subscription streams (#42794)
    
    Signed-off-by: chyezh <chyezh@outlook.com>
    
    * fix: clean transform subscription stream cache (#42794)
    
    Signed-off-by: chyezh <chyezh@outlook.com>
    
    * fix: format transform subscription changes (#42794)
    
    Signed-off-by: chyezh <chyezh@outlook.com>
    
    * refactor: decouple transform log meta from vchannel meta (#42794)
    
    Signed-off-by: chyezh <chyezh@outlook.com>
    
    * refactor: move delete checkpoint into transform log (#42794)
    
    Signed-off-by: chyezh <chyezh@outlook.com>
    
    * fix: handle txn deletes in transform log (#42794)
    
    Signed-off-by: chyezh <chyezh@outlook.com>
    
    ---------
    
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/mocks/streamingnode/server/wal/mock_recovery/mock_RecoveryStorage.go
+++ internal/mocks/streamingnode/server/wal/mock_recovery/mock_RecoveryStorage.go
@@ -9,6 +9,7 @@ import (
 	mock "github.com/stretchr/testify/mock"
 
 	recovery "github.com/milvus-io/milvus/internal/streamingnode/server/wal/recovery"
+	transformlog "github.com/milvus-io/milvus/internal/streamingnode/transformlog"
 
 	schemapb "github.com/milvus-io/milvus-proto/go-api/v3/schemapb"
 )
@@ -55,6 +56,53 @@ func (_c *MockRecoveryStorage_Close_Call) Return() *MockRecoveryStorage_Close_Ca
 
 func (_c *MockRecoveryStorage_Close_Call) RunAndReturn(run func()) *MockRecoveryStorage_Close_Call {
 	_c.Run(run)
+	return _c
+}
+
+// TransformLog provides a mock function with no fields
+func (_m *MockRecoveryStorage) TransformLog() transformlog.Accesser {
+	ret := _m.Called()
+
+	if len(ret) == 0 {
+		panic("no return value specified for TransformLog")
+	}
+
+	var r0 transformlog.Accesser
+	if rf, ok := ret.Get(0).(func() transformlog.Accesser); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(transformlog.Accesser)
+		}
+	}
+
+	return r0
+}
+
+// MockRecoveryStorage_TransformLog_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'TransformLog'
+type MockRecoveryStorage_TransformLog_Call struct {
+	*mock.Call
+}
+
+// TransformLog is a helper method to define mock.On call
+func (_e *MockRecoveryStorage_Expecter) TransformLog() *MockRecoveryStorage_TransformLog_Call {
+	return &MockRecoveryStorage_TransformLog_Call{Call: _e.mock.On("TransformLog")}
+}
+
+func (_c *MockRecoveryStorage_TransformLog_Call) Run(run func()) *MockRecoveryStorage_TransformLog_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run()
+	})
+	return _c
+}
+
+func (_c *MockRecoveryStorage_TransformLog_Call) Return(_a0 transformlog.Accesser) *MockRecoveryStorage_TransformLog_Call {
+	_c.Call.Return(_a0)
+	return _c
+}
+
+func (_c *MockRecoveryStorage_TransformLog_Call) RunAndReturn(run func() transformlog.Accesser) *MockRecoveryStorage_TransformLog_Call {
+	_c.Call.Return(run)
 	return _c
 }
 
